f2019ca3866a42ac9aeea740c3236ee99fa3961d
bablimbu <babinlimbu27@gmail.com>
2024-01-13 12:41:06 +0530

    some changess babin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Header from "@/Components/Header";
 import Hero from "@/Components/Hero";
+import Footer from "@/Components/footer";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
    <Header/>
    
 <Hero/>
+<Footer/>
    </>
   )
 }
